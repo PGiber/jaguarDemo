aa4f20173e87d5f81047ca480344624f6e88cb75
benwainwright <bwainwright28@gmail.com>
2015-10-15 16:16:42 +0100

    Initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include "hello.h"
+
+int main(int argc, char* argv[])
+{
+    if(argc > 1) {
+        printf("Hello %s", argv[1]);
+    }
+
+    else {
+        printf("Hello world!");
+    }
+    return 0;
+}
